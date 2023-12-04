cf53ab9997e2545ea5ea6fd590a935fc62f2eceb
Your Name <you@example.com>
2023-12-04 20:26:11 +0300

    navigate-gameover screen

--- App.js
+++ App.js
@@ -4,20 +4,30 @@ import GameStartScreen from "./screens/GameStartScreen";
 import { LinearGradient } from "expo-linear-gradient";
 import { useState } from "react";
 import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(null);
+  const [gameIsOver, setGameIsOver] = useState(true)
 
   const sendedNumberHandler = (sendedNumber) => {
     setUserNumber(sendedNumber);
+    setGameIsOver(false)
   };
+
+const gameOverHandler = () =>{
+  setGameIsOver(true)
+}
 
   let screen = <GameStartScreen onSendNumber={sendedNumberHandler} />;
 
   if (userNumber) {
-    screen = <GameScreen userNumber={userNumber} />;
+    screen = <GameScreen userNumber={userNumber} onGameOver={gameOverHandler} />;
   }
 
+  if(gameIsOver && userNumber){
+    screen = <GameOverScreen/>
+  }
   return (
     <LinearGradient
       style={styles.container}
--- screens/GameScreen.jsx
+++ screens/GameScreen.jsx
@@ -1,13 +1,22 @@
-import { StyleSheet, Text, View } from "react-native";
-import React, { useState } from "react";
+import { Alert, StyleSheet, Text, View } from "react-native";
+import React, { useEffect, useState } from "react";
 import Title from "../components/Title";
 import PcNumber from "../components/PcNumber";
+import CustomButton from "../components/CustomButton";
 
-export default function GameScreen({ userNumber }) {
+let minNumber = 1;
+let maxNumber = 100;
+
+export default function GameScreen({ userNumber, onGameOver }) {
   const initialGuess = generateNumber(1, 100, userNumber);
   const [currentGuess, setCurrentGuess] = useState(initialGuess);
-console.log(currentGuess);
-  
+  console.log(currentGuess);
+
+  useEffect(() => {
+    if (currentGuess === userNumber) {
+      onGameOver();
+    }
+  }, [currentGuess, userNumber, onGameOver]);
 
   function generateNumber(min, max, exclude) {
     const randomNumber = Math.floor(Math.random() * (max - min)) + min;
@@ -19,12 +28,40 @@ console.log(currentGuess);
     }
   }
 
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < userNumber) ||
+      (direction === "greater" && currentGuess > userNumber)
+    ) {
+      Alert.alert("Ooops!", "You know it's wrong...", [
+        { text: "OK", style: "cancel" },
+      ]);
+      return;
+    }
+
+    if (direction === "lower") {
+      maxNumber = currentGuess;
+    } else {
+      minNumber = currentGuess + 1;
+    }
+    const newRandomNumber = generateNumber(minNumber, maxNumber, currentGuess);
+    setCurrentGuess(newRandomNumber);
+  };
+
   return (
     <View style={styles.container}>
       <Title>Computer Guess</Title>
       <PcNumber>{currentGuess}</PcNumber>
       <View>
         <Text>Increase or Decrease </Text>
+        <View>
+          <CustomButton onPress={nextGuessHandler.bind(this, "lower")}>
+            -
+          </CustomButton>
+          <CustomButton onPress={nextGuessHandler.bind(this, "greater")}>
+            +
+          </CustomButton>
+        </View>
       </View>
     </View>
   );
