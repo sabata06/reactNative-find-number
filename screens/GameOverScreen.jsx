417fa32e505e8f177a669997e77a9a3f3dffac25
Your Name <you@example.com>
2023-12-06 00:28:10 +0300

    game screen updated

--- screens/GameOverScreen.jsx
+++ screens/GameOverScreen.jsx
@@ -1,25 +1,52 @@
-import { Image, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Title from './../components/Title';
+import { Image, StyleSheet, Text, View } from "react-native";
+import React from "react";
+import Title from "./../components/Title";
+import CustomButton from './../components/CustomButton';
 
-export default function GameOverScreen() {
+export default function GameOverScreen({ userNumber }) {
   return (
     <View style={styles.container}>
       <Title>GAME OVER</Title>
-      <View>
-        <Image source={require("../assets/success.jpg")}/>
+      <View style={styles.imgView}>
+        <Image style={styles.img} source={require("../assets/success.jpg")} />
       </View>
+      <Text style={styles.result}>
+        You found the number {userNumber} with{" "}
+        <Text style={styles.number}>10</Text> attempts
+      </Text>
+      <CustomButton>
+        New Game
+      </CustomButton>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    padding:20,
-    flex:1,
-    alignItems:"center",
-    justifyContent:"center"
-
-
-  }
-})
+  container: {
+    padding: 20,
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  imgView: {
+    width: 350,
+    height: 350,
+    overflow: "hidden",
+    borderRadius: 175,
+    borderWidth: 3,
+    borderColor: "red",
+    margin: 30,
+  },
+  img: {
+    width: "100%",
+    height: "100%",
+  },
+  result: {
+    fontSize: 20,
+    textAlign: "center",
+    marginBottom:20,
+  },
+  number: {
+    color: "red",
+  },
+});
